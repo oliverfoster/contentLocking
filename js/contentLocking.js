c7ad7bb7e6722252f067242af9f614892685ebf1
Sal Ali <salamatmoali@gmail.com>
2015-05-21 14:13:36 +0100

    fixed require

--- js/contentLocking.js
+++ js/contentLocking.js
@@ -2,7 +2,6 @@ define(function(require) {
 
 	var Adapt = require('coreJS/adapt');
 	var Backbone = require('backbone');
-	var LogosView = require('extension/branding/js/logosView');
 
 	Adapt.once('adapt:initialize', function() {
 		var contentLocking = Adapt.course.get('_contentLocking');
